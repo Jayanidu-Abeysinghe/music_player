aa05890a0da22215a115c4cf8b5ba65d6d4d3bd4
Jayanidu-Abeysinghe <jayanidu1011@gmail.com>
2025-02-12 22:58:35 +0530

    created back button , menu and album cover photo

--- lib/song_page.dart
+++ lib/song_page.dart
@@ -4,7 +4,7 @@ import 'package:music_player/new_box.dart';
 class SongPage extends StatefulWidget {
   const SongPage({super.key});
 
-  @override
+ @override
   State<SongPage> createState() => _SongPageState();
 }
 
@@ -12,6 +12,8 @@ class _SongPageState extends State<SongPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+
 
       backgroundColor: Colors.grey[300],
 
@@ -22,7 +24,8 @@ class _SongPageState extends State<SongPage> {
             children: [
 
               const SizedBox(height: 10),
-          
+
+
               //back button and menu button 
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -32,7 +35,8 @@ class _SongPageState extends State<SongPage> {
                     width: 60,
                     child: NewBox(child: Icon(Icons.arrow_back)),
                   ),
-          
+
+                  
           
                   Text('P L A Y L I S T',
                   style: TextStyle(
@@ -49,8 +53,9 @@ class _SongPageState extends State<SongPage> {
                 ],
               
               ),
-          
 
+
+              
               const SizedBox(height: 30),
             
               NewBox(
@@ -77,116 +82,3 @@ class _SongPageState extends State<SongPage> {
                                  ),
                                ),
 
-
-                               const SizedBox(height: 6,
-                               ),
-
-
-                               const Text(
-                                'Birdie',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 22,
-                                  ),
-                                 ),
-                              ],
-                            ),
-                        
-                            const Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                              size: 32,
-                              ),
-                        
-                        
-                          ],
-                        ),
-                      )
-                  ],
-                ),
-              ),
-
-
-              const SizedBox(height: 30),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-
-                Text('0.00'),
-                Icon(Icons.shuffle),
-                Icon(Icons.repeat),
-                Text('4.22'),
-              ],
-            ),
-
-
-
-            const SizedBox(height: 30),
-
-            //linear bar
-
-            NewBox(
-              child: LinearProgressIndicator(
-                minHeight: 10,
-                value: 0.40,
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-                backgroundColor: Colors.transparent,
-              ),
-            ),
-
-
-             const SizedBox(height: 30),
-
-
-
-            //previous song, pause play, skip next song 
-
-            SizedBox(
-              height: 60,
-              child: Row(
-                children: const [
-                  Expanded(
-                    child: NewBox(
-                      child: Icon(
-                        Icons.skip_previous,
-                        size: 32,
-                        )),
-                  ),
-              
-              
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.0),
-                      child: NewBox(
-                        child: Icon(
-                          Icons.play_arrow,
-                          size: 32,
-                          )),
-                    ),
-                  ),
-            
-                  Expanded(
-                    child: NewBox(
-                      child: Icon(
-                        Icons.skip_next,
-                        size: 32,
-                        )),
-                  ),
-              
-               ],
-              ),
-            )
-
-
-            ],
-          ),
-        ),
-      ),
-
-
-
-    );
-  }
-}
